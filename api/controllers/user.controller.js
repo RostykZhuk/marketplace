123cec575de1fc25b23ffdccd3fcddc0cafed24a
RostykZhuk <rostukzhuk@gmail.com>
2024-01-27 09:52:35 +0200

    create user update api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,35 @@
+import User from '../models/user.model.js';
+import { errorHandler } from '../utils/error.js';
+import bcryptjs from 'bcryptjs';
 export const test = (req, res) => {
 	res.json({
 		message: 'Testing API endpoint',
 	});
+};
+
+export const updateUser = async (req, res, next) => {
+	if (req.user.id !== req.params.id) {
+		return next(errorHandler(403, 'Forbidden'));
+	}
+	try {
+		if (req.body.password) {
+			req.body.password = bcryptjs.hashSync(req.body.password, 10);
+		}
+		const updatedUser = await User.findByIdAndUpdate(
+			req.params.id,
+			{
+				$set: {
+					username: req.body.username,
+					email: req.body.email,
+					password: req.body.password,
+					avatar: req.body.avatar,
+				},
+			},
+			{ new: true },
+		);
+		const { password, ...others } = updatedUser._doc;
+		res.status(200).json(others);
+	} catch (error) {
+		next(error);
+	}
 };
--- api/index.js
+++ api/index.js
@@ -3,9 +3,11 @@ import express from 'express';
 import mongoose from 'mongoose';
 import authRouter from './routes/auth.route.js';
 import userRouter from './routes/user.route.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 mongoose
 	.connect(process.env.MONGO_URI)
