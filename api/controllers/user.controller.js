756cd7ccea3a6c66f18f6033a55f86d6f81481d7
RostykZhuk <rostukzhuk@gmail.com>
2024-02-04 14:36:05 +0200

    create user listings api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import Listing from '../models/listing.modal.js';
 import User from '../models/user.model.js';
 import { errorHandler } from '../utils/error.js';
 import bcryptjs from 'bcryptjs';
@@ -43,5 +44,18 @@ export const deleteUser = async (req, res, next) => {
 		res.status(200).json('User has been deleted!');
 	} catch (error) {
 		next(error);
+	}
+};
+
+export const getUserListings = async (req, res, next) => {
+	if (req.user.id === req.params.id) {
+		try {
+			const listings = await Listing.find({ userRef: req.params.id });
+			res.status(200).json(listings);
+		} catch (error) {
+			next(error);
+		}
+	} else {
+		return next(errorHandler(401, 'You can only view your own listings!'));
 	}
 };
